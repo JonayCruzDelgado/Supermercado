46188151ba451e69e3cb9074d62ea5535e0e24d7
jonaycruzdelgado <jonaycruzdelgado@gmail.com>
2016-04-22 16:22:27 +0100

    habilitado rotacion

---
+++ HelloAndroid/src/es/ulpgc/eite/hello/android/landscape/LandscapeMoreProductsPresenter.java
@@ -0,0 +1,7 @@
+package es.ulpgc.eite.hello.android.landscape;
+
+/**
+ * Created by Jonay on 22/04/2016.
+ */
+public class LandscapeMoreProductsPresenter {
+}
---
+++ HelloAndroid/src/es/ulpgc/eite/hello/android/portrait/PortraitMoreProductsPresenter.java
@@ -0,0 +1,7 @@
+package es.ulpgc.eite.hello.android.portrait;
+
+/**
+ * Created by Jonay on 22/04/2016.
+ */
+public class PortraitMoreProductsPresenter {
+}
